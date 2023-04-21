f34ec200fff2057fe118d317a6674cf8abf1e695
Fireye <codekai16@gmail.com>
2023-04-20 23:57:07 -0700

    dialogue box

---
+++ Assets/DialogueBox.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : ScriptableObject
+{
+	//https://www.youtube.com/watch?v=aPXvoWVabPY
+	public string text;
+	public bool used;
+
+}
