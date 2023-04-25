6daaa3e148a9b1582b74a5a44b18921a7b5c6a8b
Fireye <codekai16@gmail.com>
2023-04-24 17:00:18 -0700

    Dialogue edits

--- Assets/DialogueBox.cs
+++ Assets/DialogueBox.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
-public class DialogueBox : MonoBehaviour
+public class DialogueBox : MonoBehaviour, IDialogueBox
 {
     //List of dialogues that will be chained together on this node.
     public List<DialogueInfo> Dialogues = new List<DialogueInfo>();
 
     public bool dialogueActive;
     public bool complete;
-    public int i;
+
+    public GameObject TextBox;
+
+    private int i;
 
     // Start is called before the first frame update
     void Awake()
@@ -18,31 +23,64 @@ public class DialogueBox : MonoBehaviour
         if (Dialogues.Count == 0) {
             complete = true;
         } else {
-            i = 0;
+            i = -1;
         }
     }
 
     public void nextDialogue() {
-        if (i < Dialogues.Count -1) {
+        if (i+1 < Dialogues.Count -1) {
             i++;
             //Todo, apply text and shit here
+        } else {
+            complete = true;
+            dialogueActive = false;
         }
     }
 
     // Update is called once per frame
     void Update() {
         if (dialogueActive) {
-			if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Mouse0) {
+			if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Mouse0)) {
                 nextDialogue();
 			}
 		}
         
     }
 
-	private void OnCollisionEnter2D(Collision2D collision) {
-		if (!complete) {
-            dialogueActive = true;
+	void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("work");
+        if (collision.gameObject.name != "Player") {
+            return;
         }
-	}
 
+        if (complete) {
+            return;
+        }
+
+        GameObject name = GameObject.Find("Text Div/Character name");
+		GameObject body = GameObject.Find("Text Div/Dialogue");
+
+        Debug.Log(name);
+        Debug.Log(body);
+
+		TextBox.SetActive(true);
+
+        dialogueActive = true;
+    }
+
+    public bool getActive() {
+        return dialogueActive;
+    }
+
+    public bool getComplete() {
+        return complete;
+    }
+
+    public void setActive(bool active) {
+        dialogueActive=active;
+    }
+
+    public void setComplete(bool completed) {
+        complete=completed;
+    }
 }
---
+++ Assets/IDialogueBox.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDialogueBox
+{
+    public void nextDialogue();
+
+    public bool getActive();
+    public bool getComplete();
+
+    public void setActive(bool active);
+    public void setComplete(bool complete);
+
+}
